2559cb11743bcebe08fa4bd0901d98edfa7a21d3
Simon Rost <simon.rost@posteo.ch>
2026-08-19 10:50:10 +0200

    Add completion

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({
+            select = false,
+          }),
+        }),
+
+        sources = {
+          {
+            name = "nvim_lsp",
+          },
+        },
+      })
+    end,
+  },
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,8 +6,13 @@ return {
 
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       vim.lsp.config("lua_ls", {
+	capabilities = capabilities,
         settings = {
           Lua = {
             runtime = {
